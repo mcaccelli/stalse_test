eb21da19d69d36cb145c257eccfc5b929b9cde98
Thiago Felipe Moreira <thiagofhold@gmail.com>
2024-06-04 10:39:41 -0300

    Estrutura - Teste Python

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+##########################################################
+########################### 01 ###########################
+##########################################################
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+##########################################################
+########################### 02 ###########################
+##########################################################
+# CRIANDO O DATAFRAME
+df = pd.DataFrame({
+    'alunos': ['Renato', 'Fernando', 'Rodrigo', 'Ana', 'Joana', 'Silvio', 'Carolina'],
+    'notas': [15.00, 39.58, 62.92, 41.46, 48.33, 63.13, 70.00]
+})
+
+# RENDERIZE OS VALORES DO DATAFRAME df EM UMA TABELA HTML DENTRO DA PÁGINA /table.html (CRIE UM HTML PARA ISSO)
+@app.route('/table')
+def table():
+
+    return render_template('table.html', )
