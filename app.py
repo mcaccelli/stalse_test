e7f309f6fe9be2faef9f565ea498f67fa120cb2c
Mateus Caccelli <mcaccelli10@gmail.com>
2025-04-15 11:46:16 -0300

    feat: adicionado método para inicialização do flask na localhost (127.0.0.1) porta 5000

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 import pandas as pd
+import os
 
 app = Flask(__name__)
+
 
 ##########################################################
 ########################### 01 ###########################
@@ -21,7 +23,23 @@ df = pd.DataFrame({
 })
 
 # RENDERIZE OS VALORES DO DATAFRAME df EM UMA TABELA HTML DENTRO DA PÁGINA /table.html (CRIE UM HTML PARA ISSO)
+
+
 @app.route('/table')
 def table():
+    return render_template('table.html', tables=[df.to_html(classes=('data', 'display'))])
 
-    return render_template('table.html', )
+
+# Foi criado um método de inicialização para a aplicação (dev ou prod) ao iniciar via python app.py
+# Caso seja em produção, é uma boa prática utilizar um serviço WSGI para Flask, inserido o GUNICORN
+if __name__ == '__main__':
+    environment = os.getenv('MODE', 'development').lower()
+
+    if environment == 'development':
+        app.run(
+            host='127.0.0.1',
+            port=5000,
+            debug=True
+        )
+    elif environment == 'production':
+        os.system("gunicorn -w 4 -b 127.0.0.1:5000 app:app")
